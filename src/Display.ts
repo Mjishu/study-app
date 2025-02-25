7da516ef4b30714707ed1c8dbb6f63167e53db8b
Mjishu <128941980+Mjishu@users.noreply.github.com>
2025-02-25 14:17:54 -0500

    ability to change background
    * can change background image
    
    * toggles off background-select parent
    
    *is not persistent through localstorage

--- src/Display.ts
+++ src/Display.ts
@@ -7,12 +7,20 @@ export class Display {
     Timer: Timer;
     newNote: HTMLButtonElement;
     audioLofi: AudioBar;
+    imageOptions: string[];
+    changeBackgroundButton: HTMLButtonElement;
+    backgroundsParent: HTMLDivElement;
+    backgroundsShown: boolean;
 
     constructor() {
         this.app = document.querySelector('#app') as HTMLElement;
         this.Timer = new Timer(true, 1500);
         this.newNote = document.querySelector('#new-note-button') as HTMLButtonElement;
         this.audioLofi = new AudioBar('/sounds/lofi-alarm.mp3');
+        this.imageOptions = ['trad_japan', 'cyberscape'];
+        this.changeBackgroundButton = document.querySelector('#show-background') as HTMLButtonElement;
+        this.backgroundsParent = document.querySelector('#background-select-parent') as HTMLDivElement;
+        this.backgroundsShown = false;
     }
 
     Start() {
@@ -27,6 +35,35 @@ export class Display {
 
     listeners() {
         this.newNote.addEventListener('click', () => this.createNote(this.app));
+        this.changeBackgroundButton.onclick = () => {
+            this.showBackgrounds();
+        };
+    }
+
+    showBackgrounds() {
+        if (this.backgroundsShown) {
+            this.backgroundsParent.style.display = 'none';
+            this.backgroundsShown = false;
+            return;
+        }
+        this.backgroundsParent.innerHTML = '';
+        this.backgroundsParent.style.display = 'flex';
+        for (const image of this.imageOptions) {
+            const button = document.createElement('button');
+            const imageUrl = `/images/${image}/${image}1920x1080.webp`;
+            button.innerHTML = `
+                <img src='${imageUrl}' alt='${image}'/>
+            `;
+            this.backgroundsParent.appendChild(button);
+            button.onclick = () => this.changeBackground(imageUrl);
+        }
+        this.backgroundsShown = true;
+    }
+
+    changeBackground(imageTitle: string) {
+        console.log('im chosen!');
+        document.body.style.backgroundImage = `url(${imageTitle})`;
+        this.backgroundsParent.style.display = 'none';
     }
 
     createNote(app: HTMLElement) {
